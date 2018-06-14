16e4d54f5ec9d175640ff889c48bb39e360b1580
awdela <1537678532@qq.com>
2018-06-14 14:49:17 +0800

    待实现

--- caimi-manager/caimi-services/src/main/java/com/caimi/service/cluster/RedisClusterMgrImpl.java
+++ caimi-manager/caimi-services/src/main/java/com/caimi/service/cluster/RedisClusterMgrImpl.java
@@ -1,10 +1,17 @@
 package com.caimi.service.cluster;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisClusterMgrImpl implements RedisClusterMgr{
 
+	@PostConstruct
+	public void init() {
+		
+	}
+	
 	@Override
 	public Object getRedis() {
 		return null;
